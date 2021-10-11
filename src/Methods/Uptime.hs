086a090d6f9911dcc0126a45819467802ae0b10b
Fred Strauss <fred@strider.co.za>
2021-10-11 20:45:55 +0200

    Add getUptime

---
+++ src/Methods/Uptime.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Methods.Uptime
+  ( getUptime
+  ) where
+
+import RPC
+import BitcoinRPCClient
+
+import Data.Text (Text)
+import Control.Monad.IO.Class (MonadIO)
+
+-- | Returns the uptime of the daemon in seconds
+getUptime :: MonadIO m => BitcoinRPCClient -> m (Either Text Int)
+getUptime client = callBitcoinRPC client "uptime" []
